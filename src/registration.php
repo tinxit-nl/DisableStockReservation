6a8a5cccc48385163e0ddfb7cac1f1572bd963d8
Rene Donkers <rene@lotsofpixels.nl>
2021-11-11 07:25:06 +0100

    Initial commit

---
+++ src/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'TinxIT_DisableStockReservation',
+    __DIR__
+);
